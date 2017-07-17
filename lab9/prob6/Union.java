e6fd731c23f3f288779827fcff511114e9003d3a
frehiwotm <fre_melk2006@yahoo.com>
2017-07-16 22:05:20 -0500

    assignment 9 and 10 changes

--- lab9/prob6/Union.java
+++ lab9/prob6/Union.java
@@ -3,20 +3,18 @@ package labs.lab9.prob6;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Union {
 	
 	public Set<String> union(List<Set<String>> sets){
 		
-		return
-				sets.stream().sequential()
-				.reduce(sets.get(0), (x, y)-> {
-						x.addAll(y);
-						return x;
-					});
-		
+		return sets.stream().reduce((x, y)-> Stream.concat(x.stream(),y.stream()).collect(Collectors.toCollection(LinkedHashSet::new))).get();
+					
 	}
 	
 	public static void main(String[] args) {
